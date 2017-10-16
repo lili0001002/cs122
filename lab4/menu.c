5bdb2beb9568d5a498be560ffa01ef6e808e7b58
LOU3501997086 <LOU3501997086@shiyanlou-no.com>
2017-10-16 16:10:12 +0800

    4

---
+++ lab4/menu.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "lintable.h"
+
+#define CMD_MAX_LEN 128
+
+typedef struct DataNode
+{
+    tLinkTableNode* pNext;
+    char* cmd;
+    char* desc;
+    void  (*handler)();
+}tDataNode;
+
+void version();
+void help();
+void printWorkDir();
+void printSystemTime();
+void list();
+void showProcess();
+void clearScreen();
+void quit();
+
+/* find the cmd of menu*/
+tDataNode* findCmd(tLinkTable* head, char* cmd)
+{
+    tDataNode* pNode = (tDataNode*)GetLinkTableHead(head);
+    while(pNode != NULL)
+    {
+        if(strcmp(pNode->cmd, cmd) == 0)
+        {
+            return pNode;
+        }
+        pNode = (tDataNode*)GetNextLinkTableNode(head, (tLinkTableNode*)pNode);
+    }
+    return NULL;
+}
+
+/* show all the cmd of menu*/
+void showAllCmd(tLinkTable* head)
+{
+    tDataNode* pNode = (tDataNode*)GetLinkTableHead(head);
+    int cnt = 0;
+    printf("  Command\t\tDescription\n");
+    while(pNode != NULL)
+    {
+         cnt++;
+         printf("%02d ", cnt);
+         printf("%s\t\t\t%s\n", pNode->cmd, pNode->desc);
+         pNode = (tDataNode*)GetNextLinkTableNode(head, (tLinkTableNode*)pNode);
+    }
+}
+
+/*initalize the data of head*/
+int InitMenuData(tLinkTable** ppLinkTable)
+{
+    *ppLinkTable = CreateLinkTable();
+    tDataNode* pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "ver";
+    pNode->desc = "show the version of menu";
+    pNode->handler = version;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "help";
+    pNode->desc = "show the version of menu";
+    pNode->handler = help;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "pwd";
+    pNode->desc = "print the working direct";
+    pNode->handler = printWorkDir;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "time";
+    pNode->desc = "print the System time and date";
+    pNode->handler = printSystemTime;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "ls";
+    pNode->desc = "list all the current direct file";
+    pNode->handler = list;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "ps";
+    pNode->desc = "show the process in the System";
+    pNode->handler = showProcess;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "clr";
+    pNode->desc = "clear the screen";
+    pNode->handler = clearScreen;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode); 
+
+    pNode = (tDataNode*)malloc(sizeof(tDataNode));
+    pNode->cmd = "quit";
+    pNode->desc = "exit from the menu program";
+    pNode->handler = quit;
+    AddLinkTableNode(*ppLinkTable, (tLinkTableNode*)pNode);
+
+    return 0;
+}
+
+tLinkTable* head = NULL;
+
+int main()
+{
+    char input[CMD_MAX_LEN];
+    InitMenuData(&head);
+
+    version();
+    printf("gitDongHub@copyright at 2017/09/24\n");
+    printf("If you need help, please type help\n");
+    while(1)
+    {
+    	printf("cmd>>");
+    	scanf("%s", input);
+    	tDataNode* p = findCmd(head, input);
+        if(p == NULL)
+        {
+            printf("the cmd you input isn't exist\n");
+            continue;
+        }
+        if(p->handler != NULL)
+        {
+            p->handler();
+        }   
+    }
+    return 0;
+}
+
+void version()
+{
+    printf("gitDongHub menu [version 2.5]\n");
+}
+
+void help()
+{
+    printf("**************************help****************************\n");
+    printf("List all the cmd of menu program:\n");
+    showAllCmd(head);
+    printf("**************************end*****************************\n");
+}
+void printWorkDir()
+{
+    system("pwd");
+}
+
+void printSystemTime()
+{
+    struct tm* ptr;
+    time_t it;
+    it = time(NULL);
+    ptr = localtime(&it);
+    printf("%4d-%02d-%02d  %d:%d:%d\n", ptr->tm_year+1900, ptr->tm_mon+1, ptr->                               tm_mday,ptr->tm_hour, ptr->tm_min, ptr->tm_sec);
+}
+
+void list()
+{ 
+    system("ls");
+}
+
+void showProcess()
+{
+    system("ps");
+}
+
+void clearScreen()
+{
+    system("clear");
+}
+
+void quit()
+{
+    exit(0);
+}
